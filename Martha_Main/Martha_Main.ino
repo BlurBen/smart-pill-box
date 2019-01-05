9f677a75bcf165e568b6ae20dc5674e6efd223c2
BlurBen <44787706+BlurBen@users.noreply.github.com>
2019-01-05 23:37:15 +0000

    Update Martha_Main.ino

--- Martha_Main/Martha_Main.ino
+++ Martha_Main/Martha_Main.ino
@@ -1,9 +1,45 @@
-void setup() {
-  // put your setup code here, to run once:
+//Change this to add timer and deep sleep mode
 
+#define TOUTCH_PIN T0 // ESP32 Pin D4
+#define LED_PIN 2
+#define SWTICH_PIN 5
+int touch_value = 100;
+bool on_value = false;
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000); // give me time to bring up serial monitor
+  Serial.println("Micro Switch Test");
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SWTICH_PIN, OUTPUT);
+  digitalWrite (LED_PIN, LOW);
+  digitalWrite (SWTICH_PIN, HIGH);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  touch_value = touchRead(TOUTCH_PIN);
+  Serial.println(touch_value);  // get value using T0
+  if (touch_value < 5)
+  {
+    switch (on_value)
+    {
+      case false:
+        on_value = true;
+        break;
+      case true:
+        on_value = false;
+        break;
+    }
+  }
+  if (on_value == true)
+  {
+    digitalWrite (LED_PIN, HIGH);
+  }
+  else
+  {
+    digitalWrite (LED_PIN, LOW);
+  }
+  delay(1000);
 }
